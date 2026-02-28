f8560ce88be2ade5f08888250dc3ea720b7ad637
Adriel <aarrcc2727@gmail.com>
2026-02-28 14:47:19 +0100

    Ejercicio 2 completado

--- ejercicio2.py
+++ ejercicio2.py
@@ -9,20 +9,31 @@ class NodeState(Struct):
     y: float= 0.0
 
 class Trazada(Struct):
-    nombre: str
-    listaPuntos: list[list[float]]
+    x: float
+    ptoMedioY: float
 
-arrayPuntos= []
+class NodeState2(Struct):
+    x: float= 0.0
+    ptoMedioY: float= 0.0
+
+global arrayPuntos
 
 @subscribe("datosCono", message_type= NodeState)
 async def recibirDatos(msg= NodeState):
     if(msg.nombre!= "FIN"):
         arrayPuntos.append([msg.x, msg.y])
     else:
+        arrayPuntos= numpy.array(arrayPuntos)
         listCoordX= numpy.unique(arrayPuntos[:,0])
-                    
-                
-        trazada= numpy.array(arrayPuntos)
-        trazada= trazada.tolist()
+        for x in listCoordX:
+            ptoMedio= 0
+            for cono in arrayPuntos:
+                if(cono[0]== x):
+                    ptoMedio= ptoMedio+cono[1]
+            # Divido entre 2 porque en el csv hay 2 valores exactos de y por cada valor de x
+            ptoMedio= ptoMedio/2
+            conoAEnviar= NodeState2(x, ptoMedio)
+            await publish("Trazada",conoAEnviar)
+            
 
 
